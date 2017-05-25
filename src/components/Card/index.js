c491f0385ed7e6b5695f24369e8be29101c814b5
Galen Lee <glee@Galens-MacBook-Pro.local>
2017-05-24 22:39:36 -0400

    updated. pls accept

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,21 @@ import './Card.css';
 import Button from '../Button'
 
 export default class Card extends Component {
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        id: PropTypes.int.isRequired,
+        moveForward: PropTypes.func.isRequired,
+        moveBackward: PropTypes.func.isRequired,
+        delete: PropTypes.func.isRequired,
+    }
+
+    static defaultProps = {
+        text: 'This is a card. It is a good card.',
+    }
+
     render() {
         return (
-            <div className='card' id={this.props.id}>
+            <div className='card'>
                 {this.props.text}
                 <Button onClick={this.props.moveBackward} variant='backward' />
                 <Button onClick={this.props.moveForward} variant='forward' />

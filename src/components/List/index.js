77c27fd5d7f5ab1b98f4885a56228f6e54f625bb
Akshitha Ramachandran <aramachandran@college.harvard.edu>
2017-05-24 22:46:03 -0400

    revised List component

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Card from '../Card/index.js';
+import Card from '../Card/';
 import './list.css';
 
-export defualt class List extends React.Component() {
-    constructor(props){
-        super(props);
-        this.props.title = props.title;
-        this.props.cards = props.cards;
-    }
+export defualt class List extends React.Component {
+    static propTypes = { id: React.PropTypes.string.isRequired,
+                         cards: React.PropTypes.array.isRequired};
+    static defaultProps = { cards: [] };
+
     render(){
         return (
             <div className="List">
                 <h3>{this.props.title}</h3>
                 <div className="Cards">
-                    {this.props.cards.map (card) => {return <Card id={card.id} text={card.text}
-                    moveForward={card.moveForward} moveBackward={card.moveBackward}/>}}
+                    {this.props.cards.map(card => <Card {...card} />)}
                 </div>
             </div>
         )
